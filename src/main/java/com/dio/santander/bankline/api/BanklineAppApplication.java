8bd8513c22ce752657c5e2178dbae30b24247420
Fernanda <fernandasoares.reis1@gmail.com>
2022-05-04 00:16:48 -0300

    first commit

---
+++ src/main/java/com/dio/santander/bankline/api/BanklineAppApplication.java
@@ -0,0 +1,13 @@
+package com.dio.santander.bankline.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/santander/bankline/api/BanklineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santander.bankline.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
