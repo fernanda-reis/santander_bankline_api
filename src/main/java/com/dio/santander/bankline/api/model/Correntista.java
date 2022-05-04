a661984ae08598bcca47fcaf6a5a54ea34c1d76e
Fernanda <fernandasoares.reis1@gmail.com>
2022-05-04 01:04:31 -0300

    mapeamento de entidades

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,13 +1,25 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_correntista")
 public class Correntista {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String cpf;
 	private String nome;
 	
+	@Embedded
 	private Conta conta;
-	
+
 	public Conta getConta() {
 		return conta;
 	}
